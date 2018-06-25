ac569849c199f35b3c4846910e56b1893b786e6d
Gaurav Jain <crazcuber@gmail.com>
2018-06-25 11:22:16 +0530

    complete pagitiona

--- api/views.py
+++ api/views.py
@@ -7,7 +7,7 @@ from oauth2_provider.contrib.rest_framework import TokenHasReadWriteScope, Token
 from .serializer import *
 from rest_framework.pagination import PageNumberPagination
 
-class BasictPagination(PageNumberPagination):
+class BasicPagination(PageNumberPagination):
     page_size_query_param = 'limit'
     max_page_size = 20
     def get_paginated_response(self, data):
@@ -23,79 +23,82 @@ class InternList(viewsets.ModelViewSet):
     permissions_classes = (permissions.IsAuthenticated,)
     queryset = Intern.objects.all()
     serializer_class = InternSerializer
-    pagination_class = BasictPagination
+    pagination_class = BasicPagination
 
 class Company_UserList(viewsets.ModelViewSet):
     permissions_classes = (permissions.IsAuthenticated,)
     queryset = Company_User.objects.all()
     serializer_class = Company_UserSerializer
+     pagination_class = BasicPagination
 
 class CategoryList(viewsets.ModelViewSet):
     permissions_classes = (permissions.IsAuthenticated,)
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+    pagination_class = BasicPagination
 
 class GithubList(viewsets.ModelViewSet):
     permissions_classes = (permissions.IsAuthenticated,)
     queryset = Github.objects.all()
     serializer_class = GithubSerializer
+    pagination_class = BasicPagination
 
 class CompanyList(viewsets.ModelViewSet):
     permissions_classes = (permissions.IsAuthenticated,)
     queryset = Company.objects.all()
-    serializer_class = CompanySerializer
+    serializer_class = CompanySerializer     
+    pagination_class = BasicPagination
 
 class SiteAdminList(viewsets.ModelViewSet):
     permissions_classes = (permissions.IsAuthenticated,permissions.IsAdminUser)
     queryset = SiteAdmin.objects.all()
-    serializer_class = SiteAdminSerializer
+    serializer_class = SiteAdminSerializer     
+    pagination_class = BasicPagination
 
 class SkillList(viewsets.ModelViewSet):
     permissions_classes = (permissions.IsAuthenticated,)
     queryset = Skill.objects.all()
-    serializer_class = SkillSerializer
+    serializer_class = SkillSerializer     
+    pagination_class = BasicPagination
 
 class DegreeList(viewsets.ModelViewSet):
     permissions_classes = (permissions.IsAuthenticated,)
     queryset = Degree.objects.all()
-    serializer_class = DegreeSerializer
+    serializer_class = DegreeSerializer     
+    pagination_class = BasicPagination
 
 class JobList(viewsets.ModelViewSet):
     permissions_classes = (permissions.IsAuthenticated,)
     queryset = Job.objects.all()
-    serializer_class = JobSerializer
+    serializer_class = JobSerializer     
+    pagination_class = BasicPagination
 
 class ProjectList(viewsets.ModelViewSet):
     permissions_classes = (permissions.IsAuthenticated,)
     queryset = Project.objects.all()
-    serializer_class = ProjectSerializer
-'''
-class HiringList(viewsets.ModelViewSet):
-    permissions_classes = (permissions.IsAuthenticated,)
-    queryset = Hiring.objects.all()
-    serializer_class = HiringSerializer
-'''
+    serializer_class = ProjectSerializer     
+    pagination_class = BasicPagination
+
 class InternshipList(viewsets.ModelViewSet):
     permissions_classes = (permissions.IsAuthenticated,)
     queryset = Internship.objects.all()
     serializer_class = InternshipSerializer
-'''
-class InternshipAvaliableList(viewsets.ModelViewSet):
-    permissions_classes = (permissions.IsAuthenticated,)
-    queryset = InternshipAvailable.objects.all()
-    serializer_class = InternshipAvaliableSerializer
-'''
+    pagination_class = BasicPagination
+
 class SubmissionList(viewsets.ModelViewSet):
     permissions_classes = (permissions.IsAuthenticated,)
     queryset = Submission.objects.all()
     serializer_class = SubmissionSerializer
+    pagination_class = BasicPagination
 
 class QuestionList(viewsets.ModelViewSet):
     permissions_classes = (permissions.IsAuthenticated,)
     queryset = Question.objects.all()
     serializer_class = QuestionSerializer
+    pagination_class = BasicPagination
 
 class AnswerList(viewsets.ModelViewSet):
     permissions_classes = (permissions.IsAuthenticated,)
     queryset = Answer.objects.all()
     serializer_class = AnswerSerializer
+    pagination_class = BasicPagination
